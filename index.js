48a58615678a3c9336c70949be7f77995d6100c6
mithu-12 <mithuvowmick96@gmail.com>
2023-11-03 20:02:30 +0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import passport from 'passport';
 import './config/passport.js';
 import expressSession from 'express-session';
 import cookieParser from 'cookie-parser';
-
+import { CyclicSessionStore } from '@cyclic.sh/session-store';
 const app = express()
 
 
@@ -26,8 +26,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const options = {
+  table: {
+    name: process.env.CYCLIC_DB,
+  }
+};
 
-app.use(expressSession({ secret: 'your-secret-key', resave: true, saveUninitialized: true, store: new expressSession.MemoryStore() }));
+app.use(expressSession({ secret: 'your-secret-key', resave: true, saveUninitialized: true, store: new CyclicSessionStore(options), }));
 app.use(passport.initialize());
 app.use(passport.session());
 
